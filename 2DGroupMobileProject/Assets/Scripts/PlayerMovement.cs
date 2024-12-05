b37b16f3ebce5687d0b99207112b4faaab621ac2
Mixmaximum <mixmaximum752@gmail.com>
2024-12-05 08:35:58 -0800

    Added input system and replaced movement code

--- 2DGroupMobileProject/Assets/Scripts/PlayerMovement.cs
+++ 2DGroupMobileProject/Assets/Scripts/PlayerMovement.cs
@@ -1,26 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    Rigidbody2D rb;
     [SerializeField]
-    float moveSpeed = 10f;
+    float moveSpeed = 5;
+    [SerializeField]
+    InputActionReference moveActionReference;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //check for horizontal and vertical input
-        //move the player based on that input
-        float xInput = Input.GetAxis("Horizontal");
-        float yInput = Input.GetAxis("Vertical");
-        //Velocity is a Vector2 variable, which stores
-        //2 floats, x and y
-        GetComponent<Rigidbody2D>().velocity = new Vector2 (xInput, yInput) * moveSpeed;
+        Vector2 moveDir = moveActionReference.action.ReadValue<Vector2>();
+        rb.velocity = moveDir * moveSpeed;
     }
 }
