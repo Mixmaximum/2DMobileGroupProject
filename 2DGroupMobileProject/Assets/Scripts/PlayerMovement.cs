2477ca99483935ba4951a0b4a3d989237a4c16b6
Mixmaximum <mixmaximum752@gmail.com>
2024-12-05 10:22:34 -0800

    Added dashing and pc/mobile movement fixed

--- 2DGroupMobileProject/Assets/Scripts/PlayerMovement.cs
+++ 2DGroupMobileProject/Assets/Scripts/PlayerMovement.cs
@@ -7,19 +7,66 @@ public class PlayerMovement : MonoBehaviour
 {
     Rigidbody2D rb;
     [SerializeField]
-    float moveSpeed = 5;
+    float moveSpeed = 5f;
     [SerializeField]
     InputActionReference moveActionReference;
+    float activeMoveSpeed;
+    public float dashSpeed = 10f;
+    public float dashLength = 0.5f;
+    public float dashCooldown = 1f;
+    float dashCounter;
+    float dashCoolCounter;
+    public TrailRenderer trail;
+
     // Start is called before the first frame update
     void Start()
     {
+        activeMoveSpeed = moveSpeed; // Initially set to moveSpeed
         rb = GetComponent<Rigidbody2D>();
+        trail = GetComponent<TrailRenderer>();
+        trail.emitting = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         Vector2 moveDir = moveActionReference.action.ReadValue<Vector2>();
-        rb.velocity = moveDir * moveSpeed;
+        rb.velocity = moveDir * activeMoveSpeed;
+        if (Input.GetButtonDown("Jump"))
+        {
+            Dash();
+        }
+
+        // Handle dash duration
+        if (dashCounter > 0)
+        {
+            dashCounter -= Time.deltaTime; // Link dash to timer
+            if (dashCounter <= 0)
+            {
+                activeMoveSpeed = moveSpeed; // Reset to normal speed after dash ends
+                dashCoolCounter = dashCooldown; // Start cooldown
+            }
+        }
+
+        // Handle dash cooldown
+        if (dashCoolCounter > 0)
+        {
+            dashCoolCounter -= Time.deltaTime; // Decrease cooldown
+        }
+        if (dashCounter <= 0)
+        { 
+            trail.emitting = false;
+        }
     }
+        // Check for dash input
+       public void Dash()
+        {
+            if (dashCoolCounter <= 0 && dashCounter <= 0) 
+            {
+                activeMoveSpeed = dashSpeed; // change player speed
+                dashCounter = dashLength; // Start dash
+                trail.emitting = true;
+            }
+
+       }
 }
