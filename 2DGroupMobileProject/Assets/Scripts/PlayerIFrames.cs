8a803f2020366ba00897011eccd008f59cfd8667
Mixmaximum <mixmaximum752@gmail.com>
2024-12-05 18:23:33 -0800

    Fixed I-frames

--- 2DGroupMobileProject/Assets/Scripts/PlayerIFrames.cs
+++ 2DGroupMobileProject/Assets/Scripts/PlayerIFrames.cs
@@ -4,49 +4,53 @@ using UnityEngine;
 
 public class PlayerIFrames : MonoBehaviour
 {
-    public float invincibilityDuration = 2f; 
+    public float invincibilityDuration = 2f;
     private bool isInvincible = false;
-    private float invincibilityTimer = 0f;
+    private float invincibilityTimeRemaining = 0f; 
     public float cooldown = 1f;
-    float cooldownCounter;
+    private float cooldownCounter = 0f;
+
     // Start is called before the first frame update
     void Start()
     {
         isInvincible = false;
+        cooldownCounter = 0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (isInvincible == true)
+        
+        if (isInvincible)
         {
-            invincibilityTimer -= Time.deltaTime;
+            invincibilityTimeRemaining -= Time.deltaTime;
 
-            if (invincibilityTimer <= 0)
+            if (invincibilityTimeRemaining <= 0)
             {
                 isInvincible = false;
                 Debug.Log("No longer invincible");
                 cooldownCounter = cooldown;
             }
         }
+
         if (cooldownCounter > 0)
         {
             cooldownCounter -= Time.deltaTime;
         }
+
         if (Input.GetButtonDown("Jump"))
         {
             Invincibility();
         }
     }
+
     private void Invincibility()
     {
-        if (!isInvincible && cooldown <= 0)
+        if (!isInvincible && cooldownCounter <= 0)
         {
             isInvincible = true;
-            invincibilityTimer = invincibilityDuration;
+            invincibilityTimeRemaining = invincibilityDuration;
             Debug.Log("Can't touch this");
         }
-
     }
-    
 }
